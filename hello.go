02c86a6227b7047502b065cd09d1f6d5343a5cca
Manav Misra <manavm@visionwebsoft.com>
2020-03-25 22:28:16 -0500

    :sparkles: Add function examples.

--- hello.go
+++ hello.go
@@ -1,17 +1,27 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"math/rand"
-	"time"
-)
+import "fmt"
+
+func add(x, y int) int {
+	return x + y
+}
+
+func lastNameFirst(fname, lname string) (string, string) {
+	return lname, fname
+}
+
+func split(num int) (x, y int) {
+	x = num * 4 / 9
+	y = num - x
+
+	// Implicity returns 'x' and 'y' as named returns 👆🏽.
+	return
+}
 
 func main() {
-	n, _ := fmt.Println("hello world!")
-	fmt.Println(n, " bytes written at: ", time.Now())
+	fmt.Println(add(2, 3))
 
-	fmt.Println("And now, here's something random: ", rand.Intn(10))
+	fmt.Println(lastNameFirst("Manav", "Misra"))
 
-	fmt.Println("How about some pi?", math.Pi)
+	fmt.Println(split(17))
 }
