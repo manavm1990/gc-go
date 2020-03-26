de7069b72c91e464309198064d84f6f2c24bdf06
Manav Misra <manavm@visionwebsoft.com>
2020-03-26 17:03:59 -0500

    ✨Use 'Printf' to show type.

--- hello.go
+++ hello.go
@@ -1,59 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"math/cmplx"
-)
-
-// Globals wrapped in 'var' block like 'import' 👆🏽
-var (
-	toBe bool = false
-
-	// Globals allow code pollution! 👎🏽
-	pollution string
-
-	// (https://stackoverflow.com/a/60866548/1653236)
-	maxValuePositiveInt uint64 = 1<<64 - 1
-
-	// Imaginary numbers - advanced math stuff 😕
-	z complex128 = cmplx.Sqrt(-5 + 12i)
-
-	defaultString string
-)
-
-func add(x, y int) int {
-	return x + y
-}
-
-func lastNameFirst(fname, lname string) (string, string) {
-	return lname, fname
-}
-
-func split(num int) (x, y int) {
-	x = num * 4 / 9
-	y = num - x
-
-	// Implicitly returns 'x' and 'y' as named returns 👆🏽.
-	return
-}
+import "fmt"
 
 func main() {
-	num1, num2 := 18, 22
-	fname, lname := "Manav", "Misra"
-
-	fmt.Println(add(num1, num2), toBe, maxValuePositiveInt)
-
-	/**
-	* Developer's Note: Unable to pass in addl. arguments b/c 'lastNameFirst' returns multiple values.
-	* This raises error regarding 'multiple values in single-value context.'
-	 */
-	fmt.Println(lastNameFirst(fname, lname))
-
-	fmt.Println(split(num1))
-
-	/**
-	* %q wraps quotes when printing strings
-	* \n creates line break and avoids '%' at EOL
-	 */
-	fmt.Printf("%v %v %q %v %q\n", num1, num2, fname, toBe, defaultString)
+	i := 30
+	fmt.Printf("%T\n", i)
 }
