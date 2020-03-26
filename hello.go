de7fabe9dc625c899bce5ef11d62fe71e1cc078b
Manav Misra <manavm@visionwebsoft.com>
2020-03-26 06:06:51 -0500

    :pencil2: Fix typo in comment.

--- hello.go
+++ hello.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// package-level variables ('global' - 🙅🏽‍♂️)
+var fname, lname string = "Manav", "Misra"
+
 func add(x, y int) int {
 	return x + y
 }
@@ -14,14 +17,13 @@ func split(num int) (x, y int) {
 	x = num * 4 / 9
 	y = num - x
 
-	// Implicity returns 'x' and 'y' as named returns 👆🏽.
+	// Implicitly returns 'x' and 'y' as named returns 👆🏽.
 	return
 }
 
 func main() {
 	num1, num2 := 18, 22
-	fname, lname := "Manav", "Misra"
-
+	var num1, num2 int = 18, 22
 	fmt.Println(add(num1, num2))
 
 	fmt.Println(lastNameFirst(fname, lname))
