598062a56ca41ffcd3dc28f94b800b52a6d41228
Manav Misra <manavm@visionwebsoft.com>
2020-03-28 07:57:33 -0500

    :poop: Scope a variable inside of 'if.'
    I don't care for this.

--- hello.go
+++ hello.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func card(age int) string {
-	if age < 21 {
-		return "No kids allowed!"
+	// We can optionally initialize a 'scoped' variable with our 'if' 😑
+	if msg := "There's a 2 drink 🍹 minimum 2nite!"; age >= 21 {
+		return msg + " 🙆🏽‍♂️, Let's party! 👯‍♂️"
+	} else { // 'scoped' 'if' variable 👆🏽is available in 'else.'
+		return "No kids allowed! But, tell your parents to come by and LTK: " + msg
 	}
-	return "🙆🏽‍♂️, Let's party! 👯‍♂️"
+
 }
 
 func main() {
