ed626310b7b0ee5a01107e93894ece6268933b7e
Manav Misra <manavm@visionwebsoft.com>
2020-03-28 06:56:46 -0500

    :sparkles: Show simple loop that omits both 'init' and 'post.'

--- hello.go
+++ hello.go
@@ -2,21 +2,16 @@ package main
 
 import "fmt"
 
-// Given a 'start' and 'stop,' continually double the numbers - 1, 2, 4, 8, 16...
-func exponentialSummer(start, stop int) (total int) {
-	// Using named return value 'total' 👆🏽prevents ugly 'out of scope' variable! 🤓
-	/**
-	* init and post statements are optional.
-	* Here, we 'init' with 'start', but don't bother with a 'post' or 'incrementor'
-	* The 'increment' is just adding 'total' to itself instead of '++'.
-	 */
-	for total = start; total <= stop; {
-		total += total
+func exponentialSummerFrom0(stop int) (total int) {
+	// If just starting from 0 and incrementing by 1s, we can omit both 'init' and 'post'.
+	// Behold! Ur beloved 'while' loop.
+	for total <= stop {
+		total++
 	}
 
 	return
 }
 
 func main() {
-	fmt.Println(exponentialSummer(1, 10))
+	fmt.Println(exponentialSummerFrom0(10))
 }
