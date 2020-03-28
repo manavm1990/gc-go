8146ee9f2aaac1bd3515c2fd1f77ba24be5b6131
Manav Misra <manavm@visionwebsoft.com>
2020-03-28 06:50:52 -0500

    :sparkles: Show 'exponential summer' with omission of 'post'

--- hello.go
+++ hello.go
@@ -2,16 +2,21 @@ package main
 
 import "fmt"
 
-// Given a 'start' and 'stop,' sum all of the numbers in between, inclusively
-func numberSummer(start, stop int) (total int) {
+// Given a 'start' and 'stop,' continually double the numbers - 1, 2, 4, 8, 16...
+func exponentialSummer(start, stop int) (total int) {
 	// Using named return value 'total' 👆🏽prevents ugly 'out of scope' variable! 🤓
-	for i := start; i <= stop; i++ {
-		total += i
+	/**
+	* init and post statements are optional.
+	* Here, we 'init' with 'start', but don't bother with a 'post' or 'incrementor'
+	* The 'increment' is just adding 'total' to itself instead of '++'.
+	 */
+	for total = start; total <= stop; {
+		total += total
 	}
 
 	return
 }
 
 func main() {
-	fmt.Println(numberSummer(1, 10))
+	fmt.Println(exponentialSummer(1, 10))
 }
