5f491c0e3ead32c361db83e1ef0b2f504a26fabd
Manav Misra <manavm@visionwebsoft.com>
2020-03-28 12:32:08 -0500

    Show stacked defers.

--- hello.go
+++ hello.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	// Stacked 'defer's
 	defer fmt.Println("Goodbye!")
+	defer fmt.Println("Goodbye2!")
+	defer fmt.Println("Goodby3!")
 
 	fmt.Println("Hello")
 }
