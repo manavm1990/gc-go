db1947a09d8d85b8ffe493bd36c6db814618e533
Manav Misra <manavm@visionwebsoft.com>
2020-03-31 10:33:27 -0500

    Show Array mutations via slices.

--- Slices/slices.go
+++ Slices/slices.go
@@ -7,7 +7,12 @@ func main() {
 	a := [6]int{1, 2, 4, 5, 6, 7}
 
 	// Starts at index 1 👆🏽UP TO (not including) index 3.
-	s := a[1:3]
+	s := a[1:5]
+	anotherSlice := a[0:4]
 
-	fmt.Println(a, s)
+	// Mutate the Array via its slice
+	// All slices referencing elements are update b/c they are just proxies for the underlying Array subsets.
+	s[1] = 333
+
+	fmt.Println(a, s, anotherSlice)
 }
