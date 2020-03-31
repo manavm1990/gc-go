a8d35d2f7d1b44f106f31c5d1b723511e8ca79fa
Manav Misra <manavm@visionwebsoft.com>
2020-03-31 10:07:38 -0500

    Add various examples for structs and pointers.

--- hello.go
+++ hello.go
@@ -2,14 +2,30 @@ package main
 
 import "fmt"
 
-// Structs
+// Structs and Pointers
 func main() {
 	type coordinates struct {
 		x int
 		y int
 	}
 
+	// Cannot do pointers with 'const'! 🙅🏽‍♂️
+	const y = 236
+
+	x := 10
+	pointerToX := &x
+
 	test := coordinates{1, 2}
+	pointerToTest := &test
+
+	test.x = 5
+
 	fmt.Println(test)
 	fmt.Println(test.x)
+
+	// Current 'state' of 'struct' for pointer to 'custom' type
+	fmt.Printf("pointerToTest has a value of: %v and a type of: %T", pointerToTest, pointerToTest)
+
+	// A memory address for pointer to 'built-in' type
+	fmt.Println(pointerToX)
 }
