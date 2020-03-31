399133fbab33443bd92ee9eb05157aefbc2d8372
Manav Misra <manavm@visionwebsoft.com>
2020-03-31 10:37:19 -0500

    Add 'on the fly' slice example.

--- Slices/slices.go
+++ Slices/slices.go
@@ -6,6 +6,9 @@ func main() {
 	// Slices provide a dynamic 'subset' of an underlying, inflexible Array
 	a := [6]int{1, 2, 4, 5, 6, 7}
 
+	// Use a 'literal' to create an Array and Slice 'on the fly.'
+	onTheFly := []string{"hello", "bye"}
+
 	// Starts at index 1 👆🏽UP TO (not including) index 3.
 	s := a[1:5]
 	anotherSlice := a[0:4]
@@ -14,5 +17,8 @@ func main() {
 	// All slices referencing elements are update b/c they are just proxies for the underlying Array subsets.
 	s[1] = 333
 
-	fmt.Println(a, s, anotherSlice)
+	fmt.Println(a, s, anotherSlice, onTheFly)
+
+	// 'onTheFly' is a slice of string type
+	fmt.Printf("%v %T\n", onTheFly, onTheFly)
 }
