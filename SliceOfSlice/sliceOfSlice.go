56f704dd1816f4a504655e4172319d6d3109dc1d
Manav Misra <manavm@visionwebsoft.com>
2020-04-04 07:01:42 -0500

    Show slice of slice.

--- SliceOfSlice/sliceOfSlice.go
+++ SliceOfSlice/sliceOfSlice.go
@@ -1,7 +1,15 @@
 package main
 
-func main() {
-	// Create a slice 'directly' from an Array
-	s := []string{"_", "_", "_"}
+import "fmt"
 
+func main() {
+	/**
+	* [] - designates a slice (no length specified, so not an array)
+	* []string - designates that 👆🏽slice will hold string slices.
+	* {} - create an underlying array of string slices that have an underlying array of 'string underscores.'
+	 */
+	ticTacToe := [][]string{[]string{"1", "2", "3"}, []string{"4", "5", "6"}, []string{"7", "8", "9"}}
+
+	// fmt.Printf("ticTacToe has length: %v and capacity of: %v\n", len(ticTacToe), cap(ticTacToe))
+	fmt.Println(ticTacToe[0][2])
 }
